53d0df10b48e5c94e0c930464a8b88664022a4fd
Theggan <moorgan.t@gmail.com>
2013-09-18 16:02:48 +0200

    Creation of the SpaceInvaders class

---
+++ src/fr/iutvalence/java/mp/SpaceInvaders/SpaceInvaders.java
@@ -0,0 +1,11 @@
+package fr.iutvalence.java.mp.SpaceInvaders;
+
+/**
+ * class SpaceInvaders : launch menu with options
+ * @author thevenim
+ */
+public class SpaceInvaders 
+{
+    SpaceInvaders[] grille = null;
+    
+}
