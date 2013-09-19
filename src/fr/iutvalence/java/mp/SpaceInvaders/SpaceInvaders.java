db9343729f3f80dcf509abbd9e2a483496dc73dc
sebastienjean <baz.jean@gmail.com>
2013-09-19 21:47:54 +0200

    Added some TODOs
    These TODOs must be processed before doing anything else

--- src/fr/iutvalence/java/mp/SpaceInvaders/SpaceInvaders.java
+++ src/fr/iutvalence/java/mp/SpaceInvaders/SpaceInvaders.java
@@ -1,11 +1,15 @@
 package fr.iutvalence.java.mp.SpaceInvaders;
 
+//TODO (fix) Fix comment, this class is everything but a launch menu
 /**
  * class SpaceInvaders : launch menu with options
  * @author thevenim
  */
 public class SpaceInvaders 
 {
+    // TODO (fix) attribute has no visibility declared
+    // TODO (fix) non static attribute should be initialized inside a constructor
+    // TODO (fix) rename attribute (english)
     SpaceInvaders[] grille = null;
     
 }
