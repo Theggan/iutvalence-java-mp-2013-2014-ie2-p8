19b1d93cc297f5cbd2c1c2d5ceaa5a413435ce24
Theggan <moorgan.t@gmail.com>
2013-10-02 16:04:47 +0200

    Creation of a main in order to see what we do, and we start to place the elements !
    Thank you, have a nice day :) !

--- src/fr/iutvalence/java/mp/SpaceInvaders/SpaceInvaders.java
+++ src/fr/iutvalence/java/mp/SpaceInvaders/SpaceInvaders.java
@@ -7,43 +7,82 @@ package fr.iutvalence.java.mp.SpaceInvaders;
  */
 public class SpaceInvaders 
 { 
-    // TODO (fix) write a comment 
-    public final static int CONSTANT_GRID = 33;
+    // TODO (fixed) write a comment
+    /**
+     * Declaration of the grid cell numbers
+     */
+    public final static int CONSTANT_GRID = 15;
 
-    // TODO (fix) write a comment 
-    public final static int CONSTANT_SPACECRAFT = 777;
-    
-    // TODO (fix) write a comment 
-    public final static int CONSTANT_ENNEMY = 666;
-    
-    // TODO (fix) write a comment 
+    // TODO (fixed) write a comment
+    /**
+     * Declaration of the value which represent the Spacecraft
+     */
+    public final static int CONSTANT_SPACECRAFT = 7;
+
+    // TODO (fixed) write a comment 
+    /**
+     * Declaration of the value which represent the Ennemy
+     */
+    public final static int CONSTANT_ENNEMY = 6;
+
+    // TODO (fixed) write a comment 
+    /**
+     * Declaration of the value which represent the Fire
+     */
     public final static int CONSTANT_FIRE = 1;
-    
-    // TODO (fix) write a comment 
-    public final static int CONSTANT_EMPTY = 0;
-    
-    // TODO (fix) write a comment 
-    public final static int CONSTANT_PLACE_SPACECRAFT_X = 32; /* The lines */
-    
-    // TODO (fix) write a comment 
-    public final static int CONSTANT_PLACE_SPACECRAFT_Y = 16; /* The columns */ 
 
-    // TODO (fix) write a comment 
+    // TODO (fixed) write a comment 
+    /**
+     * Declaration of the value which represent the Void
+     */
+    public final static int CONSTANT_EMPTY = 0;
+
+    // TODO (fixed) write a comment 
+    /**
+     * Declaration of the lines
+     */
+    public final static int CONSTANT_PLACE_SPACECRAFT_X = 14;
+
+    // TODO (fixed) write a comment
+    /**
+     * Declaration of the value which represent the columns
+     */
+    public final static int CONSTANT_PLACE_SPACECRAFT_Y = 7;
+
+    // TODO (fixed) write a comment
+    /**
+     * Declaration of the Grid
+     */
     private int[][] grid;
-    
-    // TODO (fix) write a comment 
+
+    // TODO (fixed) write a comment
+    /**
+     * Declaration of the Spacecraft
+     */
     private int spacecraft;
-    
-    // TODO (fix) write a comment 
+
+    // TODO (fixed) write a comment
+    /**
+     * Declaration of the Ennemy
+     */
     private int ennemy;
-    
-    // TODO (fix) write a comment 
+
+    // TODO (fixed) write a comment 
+    /**
+     * Declaration of the Fire
+     */
     private int fire;
-    
-    // TODO (fix) write a comment 
+
+    // TODO (fixed) write a comment 
+    /**
+     * Declaration of the Void
+     */
     private int empty;
-    
-    // TODO (fix) write a comment 
+
+    // TODO (fixed) write a comment 
+    /**
+     * Declaration of the default constructor
+     */
     public SpaceInvaders()
     {   
         this.grid = new int [CONSTANT_GRID][CONSTANT_GRID];
@@ -51,7 +90,74 @@ public class SpaceInvaders
         this.ennemy = CONSTANT_ENNEMY;
         this.fire = CONSTANT_FIRE;
         this.empty = CONSTANT_EMPTY;
+        // TODO créer une fonction pour alléger le programme qui parcours le tableau
+        //        static void parcours(int i, int j, int k, int l)
+        //        {
+        //          for(String str : tabBis)
+        //            System.out.println(str);
+        //        }
+
+
+
+
+
+
+
+
+        // fill the grid with "empty" value
+        for(int x=0; x<15; x++)
+        {
+            for (int y=0; y<15; y++)
+            {
+                this.grid[x][y]= CONSTANT_EMPTY;   
+            }
+        }
         this.grid[CONSTANT_PLACE_SPACECRAFT_X][CONSTANT_PLACE_SPACECRAFT_Y]=CONSTANT_SPACECRAFT;
-        this.grid[0][0] = CONSTANT_ENNEMY;
+
+        // put ennemies
+        for(int x=0; x<3; x++)
+        {
+            for(int y = 1; y<14; y++)
+            {
+                this.grid[x][y] = CONSTANT_ENNEMY;
+            }
+        }
+        // display the ennemies' grid
+        while(true)
+        {
+            for(int x=0; x<15; x++)
+            {
+                for(int y = 0; y<15; y++)
+                {
+                    if (this.grid[x][y]==CONSTANT_EMPTY)
+                    {
+                        System.out.print("V");
+                    }
+                    else 
+                    {
+                        System.out.print(this.grid[x][y]);
+                    }    
+                 }
+                System.out.println();
+            }
+            try
+            {
+                Thread.sleep(3000);
+            }
+            catch (InterruptedException e)
+            {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+            
+            // move ennemies to right
+            for(int x=0; x<15; x++)
+            {
+                for(int y = 2; y<15; y++)
+                {
+                    this.grid[x][y]=CONSTANT_ENNEMY;
+                }
+            }            
+        } 
     }
 }
---
+++ src/fr/iutvalence/java/mp/SpaceInvaders/test.java
@@ -0,0 +1,12 @@
+package fr.iutvalence.java.mp.SpaceInvaders;
+
+public class test
+{
+
+    public static void main(String[] args)
+    {
+        // TODO Auto-generated method stub
+        new SpaceInvaders();
+    }
+
+}
