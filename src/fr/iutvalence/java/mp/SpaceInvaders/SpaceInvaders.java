0735f2a95583fbc65af4749b1f38247136f9a5b9
sebastienjean <baz.jean@gmail.com>
2013-10-02 12:11:00 +0200

    Reviewed code, added/removed some TODOs

--- src/fr/iutvalence/java/mp/SpaceInvaders/SpaceInvaders.java
+++ src/fr/iutvalence/java/mp/SpaceInvaders/SpaceInvaders.java
@@ -7,26 +7,45 @@ package fr.iutvalence.java.mp.SpaceInvaders;
  */
 public class SpaceInvaders 
 { 
+    // TODO (fix) write a comment 
     public final static int CONSTANT_GRID = 33;
+
+    // TODO (fix) write a comment 
     public final static int CONSTANT_SPACECRAFT = 777;
+    
+    // TODO (fix) write a comment 
     public final static int CONSTANT_ENNEMY = 666;
+    
+    // TODO (fix) write a comment 
     public final static int CONSTANT_FIRE = 1;
+    
+    // TODO (fix) write a comment 
     public final static int CONSTANT_EMPTY = 0;
+    
+    // TODO (fix) write a comment 
     public final static int CONSTANT_PLACE_SPACECRAFT_X = 32; /* The lines */
+    
+    // TODO (fix) write a comment 
     public final static int CONSTANT_PLACE_SPACECRAFT_Y = 16; /* The columns */ 
 
-    // TODO (fixed) attribute has no visibility declared
-    // TODO (fixed) non static attribute should be initialized inside a constructor
-    // TODO (fixed) rename attribute (english)
+    // TODO (fix) write a comment 
     private int[][] grid;
+    
+    // TODO (fix) write a comment 
     private int spacecraft;
+    
+    // TODO (fix) write a comment 
     private int ennemy;
+    
+    // TODO (fix) write a comment 
     private int fire;
+    
+    // TODO (fix) write a comment 
     private int empty;
     
+    // TODO (fix) write a comment 
     public SpaceInvaders()
-    {
-        
+    {   
         this.grid = new int [CONSTANT_GRID][CONSTANT_GRID];
         this.spacecraft = CONSTANT_SPACECRAFT;
         this.ennemy = CONSTANT_ENNEMY;
@@ -35,7 +54,4 @@ public class SpaceInvaders
         this.grid[CONSTANT_PLACE_SPACECRAFT_X][CONSTANT_PLACE_SPACECRAFT_Y]=CONSTANT_SPACECRAFT;
         this.grid[0][0] = CONSTANT_ENNEMY;
     }
-    
-    
-    
 }
